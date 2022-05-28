14f3f2a9a99cea6a730937abb8bf7b5512236fd1
ofekzil <ofekzil162@gmail.com>
2022-05-27 21:09:35 -0700

    added chromedriver

--- src/connect_ssc.py
+++ src/connect_ssc.py
@@ -1,4 +1,18 @@
 import requests
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+cwl = 'cwl'
+password = 'password'
+
+courses = []
+
+chromedriver = Service('C:\\Users\ofekz\Python\CourseScheduler\chromedriver_win32\chromedriver.exe')
+chrome = webdriver.Chrome(service=chromedriver, options=webdriver.ChromeOptions())
+chrome.get('https://cas.id.ubc.ca/ubc-cas/login?TARGET=https%3A%2F%2Fssc.adm.ubc.ca%2Fsscportal%2Fservlets%2FSRVSSCFramework')
 
 class SSCConnector:
 
@@ -8,3 +22,8 @@ class SSCConnector:
     def login():
         pass
 
+    def create_worklist():
+        pass
+
+    def add_courses():
+        pass
